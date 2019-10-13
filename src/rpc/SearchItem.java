f1554630933e17dff2719f8827b68bd752cff7c3
0d2_100004383376_zixiu <0d2_100004383376_zixiu@git.cloud.tencent.com>
2019-10-13 19:09:12 -0400

    Add login, sql database access

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -1,5 +1,7 @@
 package rpc;
 
+import db.DBConnection;
+import db.DBConnectionFactory;
 import entity.Item;
 import external.TicketMasterClient;
 import org.json.JSONArray;
@@ -11,6 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -23,17 +26,35 @@ public class SearchItem extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // allow access only if session exists
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(403);
+            return;
+        }
+
+        // optional
+        String userId = session.getAttribute("user_id").toString();
+
         double lat = Double.parseDouble(request.getParameter("lat"));
         double lon = Double.parseDouble(request.getParameter("lon"));
 
-        TicketMasterClient client = new TicketMasterClient();
-//        RpcHelper.writeJsonArray(response, client.search(lat, lon, null));
-        List<Item> items = client.search(lat, lon, null);
-        JSONArray array = new JSONArray();
-        for (Item item : items){
-            array.put(item.toJSONObject());
+        String term = request.getParameter("term");
+        DBConnection connection = DBConnectionFactory.getConnection();
+        try {
+            List<Item> items = connection.searchItems(lat, lon, term);
+
+            JSONArray array = new JSONArray();
+            for (Item item : items) {
+                array.put(item.toJSONObject());
+            }
+            RpcHelper.writeJsonArray(response, array);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
         }
-        RpcHelper.writeJsonArray(response, array);
     }
 }
 
---
+++ src/rpc/login.java
@@ -0,0 +1,66 @@
+package rpc;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class login extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DBConnection connection = DBConnectionFactory.getConnection();
+        try {
+            HttpSession session = request.getSession(false);
+            JSONObject result = new JSONObject();
+            if(session != null){
+                String userID = session.getAttribute("user_id").toString();
+                result.put("status", "ok")
+                        .put("user_id", userID)
+                        .put("name", connection.getFullname(userID));
+            }else{
+                result.put("status", "invalid session");
+                response.setStatus(403);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DBConnection connection = DBConnectionFactory.getConnection();
+        try {
+            JSONObject body = RpcHelper.readJSONObject(request);
+            String userId = body.getString("user_id");
+            String password = body.getString("password");
+
+            JSONObject result = new JSONObject();
+            if (connection.verifyLogin(userId, password)) {
+                HttpSession session = request.getSession();
+                session.setAttribute("user_id", userId);
+                session.setMaxInactiveInterval(600);
+                result.put("status", "ok")
+                        .put("user_id", userId)
+                        .put("name", connection.getFavoriteItems(userId));
+            } else {
+                result.put("status", "user do not exist");
+                response.setStatus(401);
+            }
+            RpcHelper.writeJsonObject(response, result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(500);
+        } finally {
+            connection.close();
+        }
+    }
+}
